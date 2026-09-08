8ce2aa7530888feeb5c29d6ab4b345b68677bfc4
rrrr-777 <thisarmusic11@gmail.com>
2026-09-08 14:48:43 +0630

    Dev999

--- KartikMusic/plugins/misc.py
+++ KartikMusic/plugins/misc.py
@@ -78,7 +78,7 @@ async def update_timer(length=10, sleep=12):
                 played = media.time
                 remaining = max(duration - played, 0)
                 pos = min(int((played / duration) * length), length - 1)
-                timer = "—" * pos + "◉" + "—" * (length - pos - 1)
+                timer = "▰" * pos + "❥" + "▱" * (length - pos - 1)
 
                 if remaining <= 30:
                     next = queue.get_next(chat_id, check=True)
