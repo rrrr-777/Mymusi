29d320fbc4830f6d6c719c2e80defac8001abc28
rrrr-777 <thisarmusic11@gmail.com>
2026-09-08 09:53:37 +0630

    Dev999

--- KartikMusic/plugins/ping.py
+++ KartikMusic/plugins/ping.py
@@ -41,7 +41,7 @@ async def _ping(_, m: types.Message):
     uptime = get_time(int(time.time() - boot))
     latency = round((time.time() - start) * 1000, 2)
     await sent.edit_media(
-        media=types.InputMediaPhoto(
+        media=types.InputMediavideo(
             media=config.PING_IMG,
             caption=m.lang["ping_pong"].format(
                 latency,
